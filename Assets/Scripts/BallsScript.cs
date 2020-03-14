e5c45427c8da13884c651ebe802c6397a2f2926a
Heron Rodrigues <heronrs1@gmail.com>
2020-03-14 00:14:43 -0300

    creating and rotating balls

--- Assets/Scripts/BallsScript.cs
+++ Assets/Scripts/BallsScript.cs
@@ -2,17 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BallsScript : MonoBehaviour
-{
+public class BallsScript : MonoBehaviour {
+
+    public float speed;
+
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+        transform.Rotate(new Vector3(0f, 0f, speed * Time.deltaTime));
     }
 }

5a319b972893a9995ee34c7379843f2e415eafb5
Heron Rodrigues <heronrs1@gmail.com>
2020-03-15 10:57:24 -0300

    adding platform behavior

---
+++ Assets/Scripts/PlatformScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformScript : MonoBehaviour {
+
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        transform.localScale = new Vector2(Mathf.PingPong(Time.time, 1), transform.localScale.y);
+
+        transform.Translate(new Vector2(0f, speed * Time.deltaTime));
+    }
+}

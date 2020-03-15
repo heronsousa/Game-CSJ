3eacc7e1d929210bc23f234974c223766f9dc00b
Heron Rodrigues <heronrs1@gmail.com>
2020-03-15 19:21:42 -0300

    adding point bar

---
+++ Assets/Scripts/PointBarScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointBarScript : MonoBehaviour {
+    
+    public float speed = -1;
+
+    // Update is called once per frame
+    void Update() {
+        transform.Translate(new Vector2(0f, speed * Time.deltaTime));
+    }
+}
